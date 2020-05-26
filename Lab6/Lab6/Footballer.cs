2b7a15fc4f9b0eb2bbc046f8ca9f41ce92106552
Kremenevskiy <vkremenevskiy@gmail.com>
2020-05-26 21:27:08 +0300

    Implementation of IScorer

--- Lab6/Lab6/Footballer.cs
+++ Lab6/Lab6/Footballer.cs
@@ -1,9 +1,53 @@
 using System;
 
-namespace Lab5
+namespace Lab6
 {
-    public sealed class Footballer : Sportsman
+    public sealed class Footballer : Sportsman, IScorer
     {
+
+        public bool IsScored()
+        {
+            Random scoreChance = new Random();
+            int scoreCh = scoreChance.Next(0, 2500);
+            if (ChanceToScore() >= scoreCh)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Hooray! Goooooooal!! Goooooal!!");
+                Console.WriteLine($"{Name} {Surname} SCORED !!");
+                Console.ResetColor();
+                
+                return true;
+            }
+            else if (ChanceToScore() + 100 >= scoreCh)
+            {
+                Console.WriteLine($"OMG!! It was so close!!\n" +
+                                  $"{Name} {Surname} showing nice game\n" +
+                                  $"but ball heated to the crossbar\n");
+                return false;
+            }
+            else if (ChanceToScore() + 200 >= scoreCh)
+            {
+                Console.WriteLine($"Oooofff!! The ball hit the post!!\n" +
+                                  $"{Name} {Surname} Good Try\n");
+                return false;
+            }
+            else if (ChanceToScore() + 400 >= scoreCh)
+            {
+                Console.WriteLine($"Nice try! Accurate shot by {Name} {Surname} to the targer\n" +
+                                  $"But goalkeeper caught the ball\n");
+                return false;
+            }
+            else
+                return false;
+        }
+
+        public int ChanceToScore()
+        {
+            Random gameLuck = new Random();
+
+            return gameLuck.Next(0, 100) + Agility + Speed + Strength + Stamina;
+        }
+
         public enum GameStyle
         {
             Goalkeeper,
@@ -23,6 +67,19 @@ namespace Lab5
             _speed = rand.Next(60, 70);
             _strength = rand.Next(20, 30);
             Style = gameStyle;
+        }
+
+
+        public Footballer(string name, string surname, IFindCountry.Country country, GameStyle gameStyle,
+            int agility, int stamina, int speed, int strength)
+            : base(name, surname, country)
+        {
+            Style = gameStyle;
+            
+            _agility = agility;
+            _stamina = stamina;
+            _speed = speed;
+            _strength = strength;
         }
         
         public override int Agility
