90cc99ba048914e9b54ff891fc267455142273e2
Kremenevskiy <vkremenevskiy@gmail.com>
2020-05-26 21:26:18 +0300

    Added IFindCountry Inteface

--- Lab6/Lab6/IFindCountry.cs
+++ Lab6/Lab6/IFindCountry.cs
@@ -2,6 +2,20 @@ namespace Lab6
 {
     public interface IFindCountry
     {
+        void SelectCountry(Country country);
         
+        string CountryFrom { get; set; }
+        
+        enum Country
+        {
+            Belarus,
+            Spain,
+            // Portugal,
+            Russia
+            // Jamaica,
+            // USA,
+            // France,
+            // Canada
+        }
     }
 }
