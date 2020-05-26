7ae47d23c920dba2ed0b344cdbf037819ca0f2f5
Kremenevskiy <vkremenevskiy@gmail.com>
2020-05-26 21:24:09 +0300

    Implementation of IFighter

--- Lab6/Lab6/Boxer.cs
+++ Lab6/Lab6/Boxer.cs
@@ -1,9 +1,60 @@
 using System;
+using Lab6;
 
-namespace Lab5
+namespace Lab6
 {
-    public class Boxer : Sportsman
+    public class Boxer : Sportsman, IFighter<Boxer>
     {
+
+        public float Hit()
+        {
+            float damage;
+            float coefficient = FightCoefficient;
+
+            damage = Strength * 0.1f + Stamina * 0.1f + Agility * 0.1f;
+            
+            return damage;
+        }
+
+        public bool FightAgainst(Boxer opponent)
+        {
+            int luck1 = randLuck();
+            int luck2 = randLuck();
+            
+            Console.WriteLine("\n____Battle started!____");
+            Console.WriteLine("Boxer vs Boxer\n");
+            Console.WriteLine($"{this.Name} {this.Surname} vs {opponent.Name} {opponent.Surname}");
+            Console.WriteLine("Calculating chances...");
+            Console.WriteLine("{0, -16}| {1, -13} % {2, -10}",
+                $"Boxer: {this.Name}", $"Round Luck: {luck1.ToString()}", $"| Damage: {this.Hit().ToString()}");
+            Console.WriteLine("{0, -16}| {1, -13} % {2, -10}",
+                $"Boxer: {opponent.Name}", $"Round Luck: {luck2.ToString()}", $"| Damage: {opponent.Hit().ToString()}");
+
+            float winChance1 = this.Hit() + ( luck1 * 0.05f );
+            float winChance2 = opponent.Hit() + ( luck2 * 0.05f );
+            
+            if (winChance1 > winChance2) 
+                Console.WriteLine("\nWinner : {0}", $"{this.Name} {this.Surname} | Country: {this.CountryFrom}\n");
+            else
+                Console.WriteLine("\nWinner : {0}", $"{opponent.Name} {opponent.Surname} | Country:" +
+                                                    $" {opponent.CountryFrom}\n\n");
+            
+            
+            return winChance1 > winChance2;
+        }
+
+        public float FightCoefficient { get; set; } = 3;
+        
+        public Boxer(string name, string surname, IFindCountry.Country country, int agility, int stamina, int speed, int strength)
+            : base(name, surname, country)
+        {
+            _agility = agility;
+            _stamina = stamina;
+            _speed = speed;
+            _strength = strength;
+        }
+
+
         public Boxer(string name, string surname, int weight, int height, Gender gender,
             DateTime dateOfBirth) : base(name, surname, dateOfBirth, gender, weight, height)
         {
