37009861b657abb586bfc02d8f0e9da3a0c3a115
Kremenevskiy <vkremenevskiy@gmail.com>
2020-05-26 21:25:49 +0300

    Added IRunner Inteface

--- Lab6/Lab6/IRunner.cs
+++ Lab6/Lab6/IRunner.cs
@@ -1,4 +1,17 @@
-$HEADER$namespace $NAMESPACE$
+using System;
+
+namespace Lab6
 {
-  public interface $INTERFACE$ {$END$}
+    public interface IRunner : IComparable<IRunner>
+    { 
+        float Run(float distance);
+        string CountryFrom { get; }
+        
+        static float Record = float.MaxValue;
+        static double GetTime(double distance, double speed) => distance / speed;
+        
+        string Name { get; }
+        string Surname { get; }
+        float PersonTime { get; }
+    }
 }
